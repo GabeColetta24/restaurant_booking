c0be6bc8c77a5abe1c167c4fd49be74cc9d5748a
GabeColetta24 <houfmtub@students.codeinstitute.net>
2025-04-27 15:42:29 +0100

    Add comments to views, URLs, and templates for clarity

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('book/', views.make_booking, name='make_booking'),
+    path('', views.home, name='home'),  # Home page
+    path('book/', views.make_booking, name='make_booking'),  # Booking form page
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import BookingForm
 
-# Create your views here.
+# View to display homepage
 def home(request):
     return HttpResponse("Welcome to the Restaurant Booking System!")
 
@@ -12,8 +12,12 @@ def make_booking(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
+            # Save the booking if form data is valid
             form.save()
+            # Redirect user to the booking success page
             return render(request, 'bookings/booking_success.html')
     else:
+        # If GET request, show a blank booking form
         form = BookingForm()
+    # Render the booking form template
     return render(request, 'bookings/make_booking.html', {'form': form})
