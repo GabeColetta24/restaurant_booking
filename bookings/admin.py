0f140295b94f463a283364610a115e071a8a51f0
GabeColetta24 <houfmtub@students.codeinstitute.net>
2025-04-27 16:19:09 +0100

    Customize admin panel display for bookings

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Booking
 
+# Customize how bookings are displayed in the admin panel
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'booking_date', 'booking_time', 'number_of_guests')
+
 # Register the Booking model with the admin site
-admin.site.register(Booking)
+admin.site.register(Booking, BookingAdmin)
